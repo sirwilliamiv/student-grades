95e7aeefeb04b02ebe3011b2e18dee631027953f
sirwilliamiv <wjconnolly2@gmail.com>
2016-12-01 09:10:14 -0600

    added math.min/max

---
+++ grades.js
@@ -0,0 +1,38 @@
+// Loop over an array of student grades (values from 50-100) and outputs how many of each grades there are.
+
+// A score of 50-60 is an F
+// A score of 61-70 is a D
+// A score of 71-80 is a C
+// A score of 81-90 is a B
+// A score of 91-100 is an A
+// 
+// 
+// 
+// Use console.log to output the following criteria:
+
+// How many of each grade?
+// What is the lowest grade?
+
+// Start with array of random scores in your JavaScript
+
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+var a;
+var b;
+var c;
+var d;
+var f
+
+
+for (var i = 0; i < scores.length; i++) {
+
+
+
+
+
+// What is the highest grade? console.log(Math.max(scores))
+//  What is the lowest grade? console.log(Math.min(scores))
+
+		// if(scores[i] < 60)
+		// i+= f
+		// console.log(type of f)
+}
